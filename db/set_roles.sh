d16fa250c6203fecc6e57736f7d7130e916c1157
Giang Nguyen <hgnguyen@smu.edu.sg>
2017-05-19 12:29:21 +0800

    Allow writer to create db

--- db/set_roles.sh
+++ db/set_roles.sh
@@ -3,4 +3,6 @@
 psql -a <<-EOSQL
   CREATE USER writer WITH PASSWORD '$(cat /run/secrets/writer_password)';
   CREATE USER reader WITH PASSWORD '$(cat /run/secrets/reader_password)';
+
+  ALTER USER writer CREATEDB;
 EOSQL
